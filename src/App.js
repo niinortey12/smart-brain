1201f88c3764a65487e0e00ea03b9c5be15e8c27
Alvin Tetteh <47358094+niinortey12@users.noreply.github.com>
2021-10-13 15:24:14 +0200

    Changes made to files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
@@ -11,9 +10,9 @@ import Rank from './components/Rank/Rank';
 import './App.css';
 
 //You must add your own API key here from Clarifai.
-const app = new Clarifai.App({
- apiKey: 'b0aa66d5c8cd41d195879d4c7fb1407f'
-});
+// const app = new Clarifai.App({
+//  apiKey: 'b0aa66d5c8cd41d195879d4c7fb1407f'
+// });
 
 const particlesOptions = {
   particles: {
@@ -83,19 +82,19 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models
-      .predict(
-        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-        // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
-        // so you would change from:
-        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-        // to:
-        // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        input: this.state.input
+            })
+          })
+    .then(response => response.json())
+
+    // app.models
+    //   .predict(
+    //     Clarifai.FACE_DETECT_MODEL,
+    //     this.state.input)
       .then(response => {
         console.log('hi', response)
         if (response) {
