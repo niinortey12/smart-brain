998b036efe3aac0294805c1bb7c268a9a16c434e
Alvin Tetteh <47358094+niinortey12@users.noreply.github.com>
2021-10-13 16:02:01 +0200

    Changes made to app.js

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ class App extends Component {
     //     Clarifai.FACE_DETECT_MODEL,
     //     this.state.input)
       .then(response => {
-        console.log('hello', response)
+        console.log('hi', response)
         if (response) {
           fetch('http://localhost:3000/image', {
             method: 'put',
