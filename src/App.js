68b010e772f4b6697cf4510d409d1cb2bef6cb11
Alvin Tetteh <47358094+niinortey12@users.noreply.github.com>
2021-10-21 17:16:09 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://radiant-mountain-50869.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -98,7 +98,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://radiant-mountain-50869.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
